a2587262c01fd499141a63ff45a62f346a69d986
xkarolinaxkowalska <131850165+xkarolinaxkowalska@users.noreply.github.com>
2023-04-26 08:52:53 +0200

    Update github.py

--- github.py
+++ github.py
@@ -3,3 +3,5 @@ for i in range(60,30,-3):
 for i in range(1,8):
     for j in range(1,5):
         print("To jest: ",i , "wykonanie zewnętrznej pętli i", j , "wewnętrznej pętli")
+#zmiana_branch
+#zmiana2_branch
