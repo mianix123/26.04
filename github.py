2dbdcad3cac272ec5aaf6749629f23e418bd820a
xkarolinaxkowalska <131850165+xkarolinaxkowalska@users.noreply.github.com>
2023-04-26 08:48:20 +0200

    Update github.py

--- github.py
+++ github.py
@@ -1,5 +1,11 @@
+        #petla
+
 for i in range(60,30,-3):
     print(i)
 for i in range(1,8):
     for j in range(1,5):
         print("To jest: ",i , "wykonanie zewnętrznej pętli i", j , "wewnętrznej pętli")
+
+        
+        #zmiana_kodu
+     
