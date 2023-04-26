1851b4d27434c39eae0007674757cd2ff9b1e592
mianix123 <131850174+mianix123@users.noreply.github.com>
2023-04-26 08:35:01 +0200

    Update github.py

--- github.py
+++ github.py
@@ -2,4 +2,4 @@ for i in range(50,30,-3):
     print(i)
 for i in range(1,4):
     for j in range(1,5):
-        print("To jest",i, "wykonanie zewnętrznej pętli i", j, "wewnętrznej pętli")
+        print("To jest: ",i , "wykonanie zewnętrznej pętli i", j , "wewnętrznej pętli")
